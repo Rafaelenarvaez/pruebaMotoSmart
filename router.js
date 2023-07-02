c1260f3cc5ad44e24e2bf2e7b181411cf3cecf58
rafaelenarvaez <rafaelnarvaez1510@gmail.com>
2023-07-02 13:08:11 -0400

    update6

--- router.js
+++ router.js
@@ -3,22 +3,22 @@ app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl: 'views/home/home.html',
-            StyleSheet: 'views/home/style.css',
+            StyleSheet: '/style.css',
 
         })
         .when("/todo", {
             templateUrl: 'views/home/home.todo.html',
-            StyleSheet: 'views/home/style.css',
+            StyleSheet: '/style.css',
 
         })
         .when('/redenciones', {
             templateUrl: 'views/redenciones/redenciones.html',
-            StyleSheet: 'views/redenciones/style.css',
+            StyleSheet: '/style.css',
 
         })
         .when('/redimidos', {
             templateUrl: 'views/redenciones/redimidos.html',
-            StyleSheet: 'views/redenciones/style.css',
+            StyleSheet: '/style.css',
 
         })
         .otherwise('/')
